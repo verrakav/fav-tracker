958f83520823ff6caa958da65fa4adda711c830e
verrakav <verrakav@users.noreply.github.com>
2024-09-27 17:04:37 +1000

    full markup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,79 @@
 import "./App.css";
+import {initialFavs} from "./dummyData";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <div>
+      <h1 className="banner">Fav Tracker</h1>
+      <div className="app">
+        <FavsList />
+        <AddFav />
+        <EditFav />
+      </div>
+    </div>
+  );
 }
 
-export default App;
+///LIST///
+function FavsList() {
+  return (
+    <div className="favs-container">
+      <h1 className="heading">Your Favs</h1>
+      {initialFavs.length > 0 ? (
+        <ul className="favs-list">
+          {initialFavs.map((fav, idx) => (
+            <li className="favs-item" key={fav[idx]}>
+              <h3>{fav.name}</h3>
+              <span>{fav.type}</span>
+              <p>{fav.notes}</p> <Button>select</Button>
+            </li>
+          ))}
+          <Button>Add a fav!</Button>
+        </ul>
+      ) : (
+        "Create your fav list"
+      )}
+    </div>
+  );
+}
+
+///ADD ITEM FORM///
+function AddFav() {
+  return (
+    <form className="add-fav">
+      <h1>Add a new fav</h1>
+      <label>Name</label>
+      <input type="text" />
+      <label>Type</label>
+      <input type="text" />
+      <label>Notes?</label>
+      <input type="text" />
+      <Button>ADD!</Button>
+    </form>
+  );
+}
+
+///EDIT ITEM FORM///
+function EditFav() {
+  return (
+    <form className="edit-fav">
+      <h1>Edit your fav</h1>
+      <label>New name</label>
+      <input type="text" />
+      <label>New type</label>
+      <input type="text" />
+      <label>New note</label>
+      <input type="text" />
+      <Button>EDIT!</Button>
+    </form>
+  );
+}
+
+///REUSABLES///
+function Button({children, onClick}) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
---
+++ front-end/src/dummyData.js
@@ -0,0 +1,22 @@
+export const initialFavs = [
+  {
+    name: "Aminos",
+    type: "supplements",
+    notes: "take after exercising"
+  },
+  {
+    name: "Silky Smooth",
+    type: "shampoo",
+    notes: "the bottle lasted 4 months"
+  },
+  {
+    name: "Anzac Biscuits",
+    type: "cheeky treat",
+    notes: "good with a cup of tea"
+  },
+  {
+    name: "That Goose Game",
+    type: "video game",
+    notes: "sis wanted for bday"
+  }
+];
